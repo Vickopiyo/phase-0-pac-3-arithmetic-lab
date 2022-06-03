94be3da9aea27cf451722342ba7171fb35e7fffa
Vickopiyo <victor.okoth@student.moringaschool.com>
2022-06-03 10:38:28 +0300

    Done.

---
+++ index.js
@@ -0,0 +1,29 @@
+function add(a,b){
+    return a+b
+}
+function subtract(a,b){
+    return a - b
+}
+function multiply(a,b){
+    return a * b
+}
+function divide(a,b){
+    return a/b
+}
+function increment(a){
+    a++
+    return a
+}
+function decrement(n){
+    n--
+    return n
+}
+function makeInt(a){
+   return parseInt(a, 10)
+    
+
+}
+function preserveDecimal(n){
+    return parseFloat(n)
+
+}
